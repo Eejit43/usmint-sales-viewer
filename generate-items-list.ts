dfba0b36d6eb292c749ed680a35893c74e9fd5ed
Eejit <76887639+Eejit43@users.noreply.github.com>
2024-01-14 00:09:23 -0500

    feat: make script only run for a given year

--- generate-items-list.ts
+++ generate-items-list.ts
@@ -2,66 +2,69 @@ import { parse } from 'node-html-parser';
 
 export const rootSalesDirectory = new URL('https://www.usmint.gov/about/production-sales-figures/cumulative-sales');
 
+const itemsListFile = Bun.file('items-list.json');
+
 // eslint-disable-next-line unicorn/prefer-top-level-await
 (async () => {
-    const currentYear = new Date().getFullYear();
-    const startYear = 2015;
-
-    const years = Array.from({ length: currentYear - startYear + 1 }).map((value, index) => (startYear + index).toString());
+    const year = process.argv[2];
+    if (!year) return console.error('No year specified!');
 
     const processedRootSalesDirectory = parse(await (await fetch(rootSalesDirectory)).text());
 
-    const result: Record<string, { itemId: string; programName: string; totalSold: number; latestSale: { year: string; month: string } }> = {};
+    const selectElement = processedRootSalesDirectory.querySelector(`#${year}weeks`);
+    if (!selectElement) return console.error(`Could not find data for year ${year}!`);
 
-    for (const year of years) {
-        const selectElement = processedRootSalesDirectory.querySelector(`#${year}weeks`);
-        if (!selectElement) {
-            console.error(`Could not find select element for year ${year}!`);
+    const result: Record<string, { itemId: string; programName: string; totalSold: number; latestSale: { year: string; month: string | null; week: string } }> = (await itemsListFile.exists())
+        ? await itemsListFile.json()
+        : {};
 
-            continue;
-        }
+    const optionElements = selectElement.querySelectorAll('option');
 
-        const optionElements = selectElement.querySelectorAll('option');
+    const weeks = optionElements
+        .map((option) => [option.getAttribute('value')!, option.text.trim()])
+        .filter(([week]) => week)
+        .reverse();
 
-        const weeks = optionElements.map((option) => option.getAttribute('value')).filter(Boolean) as string[];
+    for (const [index, [week, weekName]] of weeks.entries()) {
+        console.log(`Processing week of ${weekName} (${week}) (${index + 1}/${weeks.length})`);
 
-        for (const week of weeks) {
-            console.log(`Processing ${year} week ${week} (${weeks.indexOf(week) + 1}/${weeks.length})`);
+        const dataUrl = new URL(rootSalesDirectory.toString());
+        dataUrl.searchParams.set('years', year);
+        dataUrl.searchParams.set(`${year}weeks`, week);
 
-            const dataUrl = new URL(rootSalesDirectory.toString());
-            dataUrl.searchParams.set('years', year);
-            dataUrl.searchParams.set(`${year}weeks`, week);
+        const processedData = parse(await (await fetch(dataUrl)).text()); // eslint-disable-line no-await-in-loop
 
-            const processedData = parse(await (await fetch(dataUrl)).text()); // eslint-disable-line no-await-in-loop
+        const dataTable = processedData.querySelector('table');
 
-            const dataTableBody = processedData.querySelector('table tbody');
+        if (!dataTable) return console.error('Could not find data table, stopping process (are you being rate limited?)');
 
-            if (!dataTableBody) {
-                console.error(`Could not find data table body for ${year} week ${week}!`);
+        const hasExtraColumn = dataTable.querySelectorAll('thead th')[1].text === 'Program Name';
+        if (hasExtraColumn) console.log('There is an extra "Program Name" column, the first will be ignored');
+
+        const rows = dataTable.querySelectorAll('tbody tr');
+
+        for (const [index, row] of rows.entries()) {
+            const columns = [...new Set(row.querySelectorAll('td').map((column) => column.text.trim()))];
+
+            if (columns.length > 5 && hasExtraColumn) columns.shift();
+
+            if (columns.length !== 5) {
+                console.error(`Unexpected unique column count for row ${index + 1}/${rows.length} (${columns.length}), skipping row`);
 
                 continue;
             }
 
-            const rows = dataTableBody.querySelectorAll('tr');
+            const [programName, itemId, itemName, totalSold, latestSaleDate] = columns;
 
-            for (const row of rows) {
-                const columns = row.querySelectorAll('td');
-
-                const itemName = columns[2].text.trim();
-                const totalSold = Number.parseInt(columns[3].text.trim().replaceAll(',', ''));
-
-                if (result[itemName] && result[itemName].totalSold >= totalSold) continue;
-
-                result[itemName] = {
-                    itemId: columns[1].text.trim(),
-                    programName: columns[0].text.trim(),
-                    totalSold,
-                    latestSale: { year, month: columns[4].text.trim().match(/(\d{1,2})\//)?.[1] ?? 'UNKNOWN' },
-                };
-            }
-
-            await new Promise((resolve) => setTimeout(resolve, 250)); // eslint-disable-line no-await-in-loop
+            result[itemName] = {
+                itemId,
+                programName: programName.replaceAll(/ {2,}/g, ' '),
+                totalSold: Number.parseInt(totalSold.replaceAll(',', '')),
+                latestSale: { year, month: latestSaleDate.match(/(\d{1,2})\//)?.[1] ?? null, week },
+            };
         }
+
+        await new Promise((resolve) => setTimeout(resolve, 250)); // eslint-disable-line no-await-in-loop
     }
 
     Bun.write('items-list.json', JSON.stringify(result, null, 2));
